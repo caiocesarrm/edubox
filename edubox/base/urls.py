6334a2641b35a4cd8fef4603f51f3de5baf7b7e0
caio mota <caiocesarrmota@gmail.com>
2021-06-25 14:27:25 -0300

    remove unused endpoints

--- edubox/base/urls.py
+++ edubox/base/urls.py
@@ -4,13 +4,6 @@ from edubox.base import views
 
 urlpatterns = [
 
-    
-    path("test/", view=views.Test.as_view()),
-
-    path("test2/", view=views.Test2.as_view()),
-
-    path("test3/<int:pk>/", view=views.Test3.as_view()),
-
     path("posts/", view=views.PostsListCreate.as_view()),
 
     path("posts/<int:pk>/", view=views.PostDetail.as_view()),
